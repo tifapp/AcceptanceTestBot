e28a8d0ef923670a096bebbd03f275cd475c5094
mhayes853 <matthewha853@gmail.com>
2024-06-08 23:55:48 -0700

    Open PR Client

--- src/git/pull_request.rs
+++ src/git/pull_request.rs
@@ -1,21 +1,54 @@
+use std::env;
+
+use anyhow::Result;
+use reqwest::{header::CONTENT_TYPE, Client};
+use serde::Serialize;
+
+#[derive(Debug, PartialEq, Eq, Serialize)]
 pub struct GithubPullRequest {
     title: String,
-    description: String,
+    body: String,
+    #[serde(skip)]
     owner: String,
+    #[serde(skip)]
     repo: String,
-    head_branch: String,
-    base_branch: String
+    head: String,
+    base: String
 }
 
 impl GithubPullRequest {
-    pub fn for_tif_react_frontend(title: String, description: String, head_branch: String) -> Self {
+    /// Creates a PR for the main frontend repo.
+    pub fn for_tif_react_frontend(title: String, body: String, head_branch: String) -> Self {
         Self {
-            title,
-            description,
-            head_branch,
+            body,
+            title: format!("Roswaal: {}", title),
             owner: "tifapp".to_string(),
             repo: "FitnessProject".to_string(),
-            base_branch: "development".to_string(),
+            base: "development".to_string(),
+            head: format!("roswaal:{}", head_branch)
         }
+    }
+}
+
+pub trait GithubPullRequestOpen {
+    /// Opens a PR on github, and returns true if it was created successfully.
+    async fn open(&self, pull_request: &GithubPullRequest) -> Result<bool>;
+}
+
+impl GithubPullRequestOpen for Client {
+    async fn open(&self, pull_request: &GithubPullRequest) -> Result<bool> {
+        let url = format!(
+            "https://api.github.com/repos/{}/{}/pulls",
+            pull_request.owner,
+            pull_request.repo
+        );
+        let response = self.post(url)
+            .bearer_auth(env::var("GITHUB_API_KEY").unwrap())
+            .header(CONTENT_TYPE, "application/json")
+            .header("X-GitHub-Api-Version", "2022-11-28")
+            .json(&pull_request)
+            .send()
+            .await?;
+        Ok(response.status() == 201)
     }
 }
