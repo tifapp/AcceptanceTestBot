5098a8d841f8856a23706b03ab663a021b404a81
mhayes853 <matthewha853@gmail.com>
2024-06-19 17:29:07 -0700

    Get rid of storage type

--- src/location/storage.rs
+++ src/location/storage.rs
@@ -1,61 +1,43 @@
 use anyhow::Result;
 use sqlx::{prelude::FromRow, query, query_as, Sqlite};
 
-use crate::{utils::sqlite::RoswaalSqlite, with_transaction};
+use crate::utils::sqlite::RoswaalSqliteTransaction;
 
 use super::{coordinate::LocationCoordinate2D, location::RoswaalLocation, name::RoswaalLocationName};
 
-pub struct RoswaalLocationsStorage {
-    sqlite: RoswaalSqlite
-}
-
-impl RoswaalLocationsStorage {
-    pub fn new(sqlite: RoswaalSqlite) -> Self {
-        Self { sqlite }
-    }
-}
-
-const UPSERT_QUERY: &str = "
-INSERT OR REPLACE INTO Locations (latitude, longitude, name) VALUES (?, ?, ?);
-";
-
-impl RoswaalLocationsStorage {
-    pub async fn save(&self, locations: &Vec<RoswaalLocation>) -> Result<()> {
-        let mut transaction = self.sqlite.transaction().await?;
-        with_transaction!(transaction, async {
-            let statements = locations.iter()
-                .map(|_| UPSERT_QUERY)
-                .collect::<Vec<&str>>()
-                .join("\n");
-            let mut bulk_insert_query = query::<Sqlite>(&statements);
-            for location in locations.iter() {
-                bulk_insert_query = bulk_insert_query.bind(location.coordinate().latitude())
-                    .bind(location.coordinate().longitude())
-                    .bind(&location.name().raw_value)
-            }
-            bulk_insert_query.execute(transaction.connection()).await?;
-            Ok(())
-        })
-    }
-
-    pub async fn all_in_alphabetical_order(&self) -> Result<Vec<RoswaalLocation>> {
-        let mut transaction = self.sqlite.transaction().await?;
-        with_transaction!(transaction, async {
-            let locations = query_as::<Sqlite, SqliteLocation>(
-                "SELECT * FROM Locations ORDER BY name"
-            )
-            .fetch_all(transaction.connection())
-            .await?
-            .iter()
-            .map(|l| {
-                RoswaalLocation::new(
-                    RoswaalLocationName { raw_value: l.name.clone() },
-                    LocationCoordinate2D { latitude: l.latitude, longitude: l.longitude }
-                )
+impl <'a> RoswaalSqliteTransaction <'a> {
+    pub async fn save_locations(&mut self, locations: &Vec<RoswaalLocation>) -> Result<()> {
+        let statements = locations.iter()
+            .map(|_| {
+                "INSERT OR REPLACE INTO Locations (latitude, longitude, name) VALUES (?, ?, ?);"
             })
-            .collect();
-            Ok(locations)
+            .collect::<Vec<&str>>()
+            .join("\n");
+        let mut bulk_insert_query = query::<Sqlite>(&statements);
+        for location in locations.iter() {
+            bulk_insert_query = bulk_insert_query.bind(location.coordinate().latitude())
+                .bind(location.coordinate().longitude())
+                .bind(&location.name().raw_value)
+        }
+        bulk_insert_query.execute(self.connection()).await?;
+        Ok(())
+    }
+
+    pub async fn locations_in_alphabetical_order(&mut self) -> Result<Vec<RoswaalLocation>> {
+        let locations = query_as::<Sqlite, SqliteLocation>(
+            "SELECT * FROM Locations ORDER BY name"
+        )
+        .fetch_all(self.connection())
+        .await?
+        .iter()
+        .map(|l| {
+            RoswaalLocation::new(
+                RoswaalLocationName { raw_value: l.name.clone() },
+                LocationCoordinate2D { latitude: l.latitude, longitude: l.longitude }
+            )
         })
+        .collect();
+        Ok(locations)
     }
 }
 
@@ -70,13 +52,12 @@ struct SqliteLocation {
 mod tests {
     use std::str::FromStr;
 
-    use crate::location::{coordinate::LocationCoordinate2D, location::RoswaalLocation, name::RoswaalLocationName};
-
-    use super::*;
+    use crate::{location::{coordinate::LocationCoordinate2D, location::RoswaalLocation, name::RoswaalLocationName}, utils::sqlite::{RoswaalSqlite, RoswaalSqliteTransaction}};
 
     #[tokio::test]
     async fn test_add_and_load_locations_no_prior_locations() {
-        let storage = RoswaalLocationsStorage::new(RoswaalSqlite::in_memory().await.unwrap());
+        let sqlite = RoswaalSqlite::in_memory().await.unwrap();
+        let mut transaction = sqlite.transaction().await.unwrap();
         let locations = vec![
             RoswaalLocation::new(
                 RoswaalLocationName::from_str("Antarctica").unwrap(),
@@ -87,14 +68,15 @@ mod tests {
                 LocationCoordinate2D::try_new(45.0, 45.0).unwrap()
             )
         ];
-        _ = storage.save(&locations).await;
-        let saved_locations = storage.all_in_alphabetical_order().await.unwrap();
+        _ = transaction.save_locations(&locations).await;
+        let saved_locations = transaction.locations_in_alphabetical_order().await.unwrap();
         assert_eq!(locations, saved_locations)
     }
 
     #[tokio::test]
     async fn test_add_and_load_locations_upserts_prior_locations() {
-        let storage = RoswaalLocationsStorage::new(RoswaalSqlite::in_memory().await.unwrap());
+        let sqlite = RoswaalSqlite::in_memory().await.unwrap();
+        let mut transaction = sqlite.transaction().await.unwrap();
         let mut locations = vec![
             RoswaalLocation::new(
                 RoswaalLocationName::from_str("Antarctica").unwrap(),
@@ -105,7 +87,7 @@ mod tests {
                 LocationCoordinate2D::try_new(45.0, 45.0).unwrap()
             )
         ];
-        _ = storage.save(&locations).await;
+        _ = transaction.save_locations(&locations).await;
         locations = vec![
             RoswaalLocation::new(
                 RoswaalLocationName::from_str("Antarctica").unwrap(),
@@ -116,8 +98,8 @@ mod tests {
                 LocationCoordinate2D::try_new(45.0, 45.0).unwrap()
             )
         ];
-        _ = storage.save(&locations).await;
-        let saved_locations = storage.all_in_alphabetical_order().await.unwrap();
+        _ = transaction.save_locations(&locations).await;
+        let saved_locations = transaction.locations_in_alphabetical_order().await.unwrap();
         assert_eq!(
             saved_locations,
             vec![
